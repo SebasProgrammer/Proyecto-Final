0e5e0192430bc60e93a7406f1e0eccbe9661aa02
dorian <dorian11sanchez@gmail.com>
2023-04-11 01:03:14 -0500

    cambios

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+  return (
+    <div>
+      NAVBAR
+    </div>
+  )
+}
+
+export default NavBar;
---
+++ src/store/index.jsx
@@ -0,0 +1,8 @@
+//REDUX
+import { configureStore } from "@reduxjs/toolkit";
+
+export default  configureStore({
+    reducer: {
+        
+    }
+})
