af75165839848c8cf5ecefd9638e67c8603790e1
Locoarana <sebastian.arana2016@gmail.com>
2023-04-14 21:54:01 -0500

    inicio sesion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../App.css'
 
 function Login() {
   return (
     <div>
-        <h1>Login</h1>
+        <h1>Inicio Sesión</h1>
+
+        <form>
+          <div className="input-group mb-3 mx-auto">
+            <input type="text" className="form-control" placeholder="Usuario" />
+          </div>
+
+          <div className="input-group mb-3 mx-auto">
+            <input type="text" className="form-control" placeholder="Contraseña" />
+          </div>
+          <button className="btn btn-primary input-group-text" type="submit">Inicio Sesión</button>
+        </form>
     </div>
   )
 }
