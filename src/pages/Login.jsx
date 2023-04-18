232ac7d4ed4fc6fd799878c701ebdd8ba528110c
Locoarana <sebastian.arana2016@gmail.com>
2023-04-18 01:32:38 -0500

    1 hora de la 3era clase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,21 +2,39 @@ import React from 'react'
 import '../App.css'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 function Login() {
+
+  const {register, handleSubmit} = useForm()
+  const submit = data => {
+    axios
+      .post("https://e-commerce-api-v2.academlo.tech/api/v1/users/login", data)
+      .then(resp => {
+        console.log(resp.data)
+      })
+      .catch( error => {
+        console.log(error)
+      })
+  }
   return (
-    <Form>
+    <Form
+      onSubmit={handleSubmit(submit)}
+    >
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label>Correo</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" 
+        { ...register("email")}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control type="password" placeholder="Password" 
+        { ...register("password")}/>
       </Form.Group>
       <Button variant="primary" type="submit">
-        Submit
+        Iniciar Sesión
       </Button>
     </Form>
   )
